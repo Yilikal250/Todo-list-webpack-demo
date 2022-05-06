bfa9fedcbccf3e3e72fdf6f6472cee31c74883f3
Yilikal250 <teshomeyilikal250@gmail.com>
2022-05-07 01:22:12 +0300

    to do except edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,125 @@
-import'./style.css';
-import { camelCase } from 'lodash';
-console.log('hello world');
+/** @format */
+
+import './style.css';
+
+const todoForm = document.querySelector('.todo-form');
+
+const todoInput = document.querySelector('.todo-input');
+
+const todoItemsList = document.querySelector('.todo-items');
+let todos = [];
+todoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addTodo(todoInput.value);
+});
+function addTodo(item) {
+  if (item !== '') {
+    const todo = {
+      id: 0,
+      name: item,
+      completed: false,
+    };
+    todos.push(todo);
+    addToLocalStorage(todos);
+    todoInput.value = '';
+  }
+}
+
+function renderTodos() {
+  todoItemsList.innerHTML = '';
+  let index = 0;
+  todos.forEach((item) => {
+    item.id = index;
+    // const checked = item.completed ? 'checked' : null;
+    const li = document.createElement('li');
+
+    li.setAttribute('class', 'item');
+
+    li.setAttribute('data-key', item.id);
+    
+    let checked;
+
+    if (item.completed) {
+       checked = "fa-square-check"
+    } else { checked = "fa-square" }
+    li.innerHTML = `
+    <div class="task-cont">
+    <i class= "fa-regular ${checked}" id="${index}"></i>
+    <p class="para">${item.name}</p> 
+    <button id="${index}"  class="delete-button">  </button>    
+    </div>
+
+    `;
+
+    todoItemsList.append(li);
+    index +=1;
+  });
+}
+function addToLocalStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  renderTodos(todos);
+}
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+
+  if (reference) {
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
+}
+
+function toggle(id) {
+  todos.forEach((item) => {
+    if (item.id == id) {
+      item.completed = !item.completed;
+    }
+  });
+  addToLocalStorage(todos);
+}
+
+function deleteTodo(index) {
+  todos = todos.filter((item) => item.id !== index);
+  addToLocalStorage(todos);
+}
+getFromLocalStorage();
+todoItemsList.addEventListener('click', (event) => {
+  if (event.target.type === 'checkbox') {
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+  if (event.target.classList.contains('delete-button')) {
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
+
+/*const checkedBox = document.querySelectorAll(".checked");*/
+
+
+
+document.addEventListener('click', (box) => {
+  if(box.target.classList.contains("clear")) {
+
+todos = todos.filter((item) => !item.completed);
+addToLocalStorage(todos);
+  }
+if(box.target.classList.contains("fa-regular")){
+  box.target.classList.toggle("fa-square")
+  box.target.classList.toggle("fa-square-check")
+  let index = box.target.id;
+  todos[index].completed = !todos[index].completed;
+  addToLocalStorage(todos);
+
+  }
+
+  if(box.target.classList.contains("delete-button")){
+    let index = parseInt(box.target.id);
+    deleteTodo(index);
+  }
+
+  if(box.target.classList.contains("para")){
+    const editInputs = document.querySelector(".para");
+    editInputs.setAttribute("contenteditable",true);
+  }
+} )
+
+new Sortable(todoItemsList, { Animation: 400 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports ={
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'main.js',
-        clean: true,
-    },
 
-    module: {
-
-        rules: [
-    
-          {
-    
-            test: /\.css$/,
-    
-            use: ['style-loader', 'css-loader'],
-    
-          },
-    
-        ],
-    
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
-      devServer: {
-        static: './dist',
-     },
-      plugins: [
-          new HtmlWebpackPlugin({
-              title:"webpack App",
-              filename:'index.html',
-              template:'src/template.html',
-
-          }),
-      ],
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
